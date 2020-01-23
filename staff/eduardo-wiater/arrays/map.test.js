76b775508cced6d58f982f60dd9f5ab80e18542d
Eduardo Wiater <kpooh7766@gmail.com>
2020-01-23 01:32:09 +0100

    final dia

--- staff/eduardo-wiater/arrays/map.test.js
+++ staff/eduardo-wiater/arrays/map.test.js
@@ -2,21 +2,26 @@ console.log("Test Map -------------------------------------------------------")
 console.log("Devuelve un array con los elementos modificados por el callback.")
 
 var cont = [3, 4, 7, 8];
+var test = map(cont,function(x){return x + 6 });
+
+it('Should return an error if theres undefined in array', function(){
+    
+})
 
 
-var test = map(cont,function(x){return x + 6 })
 
 
 
-var _error;
 
-try {
-    map(undefined,function(){})
-} catch (error) {
-    _error = error
-} finally {
-    console.assert( _error.message === "undefine is not an array", " should fail with message undefined is not an array") 
-}
+// var _error;
 
-// console.log(map(a,function(x){return x + 2 }));
-// console.log(map(a,function(x){return x * 2 }));
+// try {
+//     map(undefined,function(){})
+// } catch (error) {
+//     _error = error
+// } finally {
+//     console.assert( _error.message === "undefine is not an array", " should fail with message undefined is not an array") 
+// }
+
+// // console.log(map(a,function(x){return x + 2 }));
+// // console.log(map(a,function(x){return x * 2 }));
