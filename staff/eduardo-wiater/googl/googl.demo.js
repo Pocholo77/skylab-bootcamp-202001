00750e973b94253dd28853fe36c63649a03da3d4
Eduardo Wiater <kpooh7766@gmail.com>
2020-01-28 09:37:55 +0100

    finished with some doubt about rating stuff

--- staff/eduardo-wiater/googl/googl.demo.js
+++ staff/eduardo-wiater/googl/googl.demo.js
@@ -1,5 +1,10 @@
-googl('pepito', function(results) { 
-    results.forEach(function(result) { 
-        console.log(result) 
-    })
-})
+googl('pepito', function (results) {
+    results.forEach(function (result) {
+        console.log(result)
+        console.assert(!(result === undefined), "it should return t0he result ");
+        if(result.rating === true){ 
+            console.assert((!(result.rating === undefined)), "it should return the rating ");
+        }         
+        console.assert(!(result.description === undefined),"it should return the description ");
+    });
+});
--- staff/eduardo-wiater/googl/googl.js
+++ staff/eduardo-wiater/googl/googl.js
@@ -1,48 +1,48 @@
 function googl(query, callback) {
-    var xhr = new XMLHttpRequest
+    var xhr = new XMLHttpRequest;
 
-    xhr.open('GET', 'https://www.google.com/search?q=' + query)
+    xhr.open('GET', 'https://www.google.com/search?q=' + query);
 
     xhr.onreadystatechange = function (res) {
         //debugger
         if (this.readyState === 4 && this.status === 200) {
             //console.log(this.responseText)
 
-            var doc = new DOMParser().parseFromString(this.responseText, 'text/html')
+            var doc = new DOMParser().parseFromString(this.responseText, 'text/html');
 
-            var items = doc.querySelectorAll('div.g')
+            var items = doc.querySelectorAll('div.g');
 
-            var results = []
+            var results = [];
 
             for (var i = 0; i < items.length; i++) {
-                var item = items[i]
+                var item = items[i];
 
-                var title = item.querySelector('h3.LC20lb')
+                var title = item.querySelector('h3.LC20lb');
 
                 if (title) {
-                    var result = {}
+                    var result = {};
 
-                    result.title = title.innerText
+                    result.title = title.innerText;
 
-                    var rating = item.querySelector('.slp.f')
+                    var rating = item.querySelector('.slp.f');
 
                     if (rating) {
-                        result.rating = rating.innerText
+                        result.rating = rating.innerText;
                     }
 
-                    var description = item.querySelector('.st')
+                    var description = item.querySelector('.st');
 
                     if(description){
-                        result.description = description.innerText
+                        result.description = description.innerText;
                     }
                 }
 
-                results.push(result)
+                results.push(result);
             }
 
-            callback(results)
+            callback(results);
         }
     }
 
-    xhr.send()
+    xhr.send();
 }
