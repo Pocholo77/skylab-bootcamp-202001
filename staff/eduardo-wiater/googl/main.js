2725918afb9577f7e79643d2aef44a08913cb0bb
Eduardo Wiater <kpooh7766@gmail.com>
2020-01-30 09:25:20 +0100

    imported manu files

--- staff/eduardo-wiater/googl/main.js
+++ staff/eduardo-wiater/googl/main.js
@@ -1,28 +1,63 @@
-var search = document.querySelector('form');
-var container = document.createElement('div');
-var ul = document.createElement('ul');
-container.appendChild(ul);
+'use strict';
 
-search.addEventListener('submit',function(event){
-    event.preventDefault();
-    
-    var query = this.query.value;
-    
-    googl(query,function(results){
-        results.forEach(function(result){
-            var li = document.createElement('li');
-            var title = document.createTextNode(result.title);
-            var h3 = document.createElement('h3');
-            var p = document.createElement('p');
-            var description = document.createTextNode(result.description);
-            h3.appendChild(title);
-            p.appendChild(description);
-            ul.appendChild(li).appendChild(h3).appendChild(p);
-        });
-        
+// LET's spy to see what's going on in stack...
+// call = spy(call);
+// search = spy(search);
+// googl = spy(googl);
+
+// main code here...
+
+var IT = '🎈🤡';
+
+var login = createLogin('login', function (username, password) {
+    var user = users.find(function (user) { return user.username === username; });
+
+    if (user && user.password === password) {
+        login.toggle();
+        _googl.toggle();
+        _ecosia.toggle();
+        _bing.toggle();
+        _yahoo.toggle();
+    } else alert('Wrong credentials, you cannot get in ' + IT);
+}, function () {
+    login.toggle();
+    _register.toggle();
+});
+
+var _register = createRegister('register', function (name, surname, username, password) {
+    try {
+        register(name, surname, username, password);
+
+        _register.toggle();
+        login.toggle();
+    } catch (error) {
+        alert(error.message + ' ' + IT);
+    }
+}, function () {
+    _register.toggle();
+    login.toggle();
+});
+
+var _googl = createSearch('search', function (query) {
+    googl(query, spy(function (results) {
+        createResults('.results', results);
+    }));
+});
+
+var _ecosia = createSearch('search-2', function (query) {
+    ecosia(query, function (results) {
+        createResults('.results-2', results);
     });
+});
 
-    search.insertAdjacentElement('afterend', container);
+var _bing = createSearch('search-3', function (query) {
+    bing(query, function (results) {
+        createResults('.results-3', results);
+    });
+});
 
-
-})
+var _yahoo = createSearch('search-4', function (query) {
+    bing(query, function (results) {
+        createResults('.results-4', results);
+    });
+});
