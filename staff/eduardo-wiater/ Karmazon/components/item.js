3510f000e5f543a869583369b1993b1afa4afcd7
Eduardo Wiater <kpooh7766@gmail.com>
2020-02-02 22:45:30 +0100

    karmazon I

---
+++ staff/eduardo-wiater/ Karmazon/components/item.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function Item(result, onClick) {
+    var item = document.createElement('div');
+
+    Component.call(this, item);
+
+    item.classList.add('item');
+
+    item.innerHTML = '<h2>' + result.name + '</h2>'
+        .concat('<img src="' + result.thumbnail + '">')
+        .concat('<span>Price: ' + result.price + '</span>');
+
+    item.querySelector('img').addEventListener('click', function() {
+        var id = result.id;
+        
+        onClick(id);
+
+    })
+}
+
+Item.prototype = Object.create(Component.prototype);
+Item.prototype.constructor = Item; 
